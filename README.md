e0a8f263d5d56ea0ab2f09aa99371e0060ec2f8a
Joakim Hovlandsvåg <joakim.hovlandsvag@usit.uio.no>
2021-08-18 12:24:15 +0200

    Initial commit of plan Docusaurus, with config mods

---
+++ README.md
@@ -0,0 +1,40 @@
+# Midlertidig datadelingsdokumentasjon
+
+Midlertidig område for prosjekt Unit Datadeling sin dokumentasjon. Denne ligg
+midlertig på https://jokim.github.io/datadeling-docs-tmp/, men skal på sikt
+migrerast til https://docs.uninett.no/docs/datadeling/.
+
+Dokumentasjonen bruker [Docusaurus 2](https://docusaurus.io/).
+
+## Installation
+
+Bruker Docusaurus sitt standard oppsett. Sjå
+https://docusaurus.io/docs/installation for meir detaljar.
+
+```console
+yarn install
+```
+
+## Local Development
+
+```console
+yarn start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+## Build
+
+```console
+yarn build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+## Deployment
+
+```console
+GIT_USER=<Your GitHub username> USE_SSH=true yarn deploy
+```
+
+If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
---
+++ docusaurus.config.js
@@ -0,0 +1,68 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Datadeling Test',
+  tagline: 'Test av dokumentasjonen til prosjekt Datadeling',
+  url: 'https://jokim.github.io/',
+  baseUrl: '/datadeling-docs-tmp/',
+  trailingSlash: false,
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'jokim', // Usually your GitHub org/user name.
+  projectName: 'datadeling-docs-tmp', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Datadeling',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Dokumentasjon',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} HK-dir, Unit, Uninett AS`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            // TODO: feil URL, lenk heller til repoet med rådata, og ikkje den
+            // ferdigbygde frå build, som ser ut itl å innehalde ferdige
+            // html-sider...
+            'https://github.com/jokim/datadeling-docs-tmp/tree/gh-pages/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
